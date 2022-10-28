be36169cc073ad735a294a526ed6e4ed45e3fa70
inon.s <inon.sharony@asterra.io>
2022-10-28 14:15:05 +0300

    Keep API clean!
    As the API evolves to support more functionalities, it becomes imprative to keep it clean.
    Routes abstraced from all handling and logic, which is moved to Controllers folder.

---
+++ server/controllers/posts.js
@@ -0,0 +1,9 @@
+// handlers
+
+export const getPosts = (req, res) => {
+    res.send('posts handled');
+}
+
+export const createPost = (req, res) => {
+    res.send('post created');
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,15 @@
 import express from 'express';
 
+/** Keep API clean!
+ * Routes only, here.
+ * Move all handling and logic to '/controllers/..."
+*/
+
+import { getPosts, createPost } from '../controllers/posts.js';
+
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.send('works');
-});
+router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router;
