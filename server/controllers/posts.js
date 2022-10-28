01ddf61d3f7eff84ed888c4854ca5757d10267fa
inon.s <inon.sharony@asterra.io>
2022-10-28 14:19:57 +0300

    Explicit data model for posts.
    * Read functionality provided by Mongoose.
    * Error handling in getPosts() .

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,15 @@
 // handlers
 
-export const getPosts = (req, res) => {
-    res.send('posts handled');
+import PostMessage from "../models/postMessage.js";
+
+
+export const getPosts = async (req, res) => {
+    try{
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
 }
 
 export const createPost = (req, res) => {
