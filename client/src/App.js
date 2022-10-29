c06007fa30c1b2c1b2c0fba55c6d7a5221b66574
inon.s <inon.sharony@asterra.io>
2022-10-29 22:50:44 +0300

    Added basic Material UI elements to client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+const memories_img_src = "https://bit.ly/memories_image";
+
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={ memories_img_src } alt="memories" height="300" />
+            </AppBar>
+
+        </Container>
     );
 }
 
